5f5d21fe2cbb63416ffde9e86600aaa56bd551eb
m-uthoki <165477815+m-uthoki@users.noreply.github.com>
2024-04-15 18:12:08 +0300

    Update code20.cpp

--- code20.cpp
+++ code20.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a binary tree node
 struct TreeNode {
     int data;
     struct TreeNode *left;
     struct TreeNode *right;
 };
 
-// Function to create a new node
 struct TreeNode* newNode(int data) {
     struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
     node->data = data;
@@ -16,7 +14,6 @@ struct TreeNode* newNode(int data) {
     return node;
 }
 
-// Function to find index of value in an array
 int findIndex(int arr[], int start, int end, int value) {
     for (int i = start; i <= end; i++) {
         if (arr[i] == value)
@@ -25,7 +22,6 @@ int findIndex(int arr[], int start, int end, int value) {
     return -1;
 }
 
-// Function to construct BST from in-order and post-order traversals
 struct TreeNode* constructUtil(int in[], int post[], int inStart, int inEnd, int* postIndex) {
     if (inStart > inEnd)
         return NULL;
@@ -44,18 +40,15 @@ struct TreeNode* constructUtil(int in[], int post[], int inStart, int inEnd, int
     return node;
 }
 
-// Function to construct BST
 struct TreeNode* bst_construct(int in[], int post[], int n) {
     int postIndex = n - 1;
     return constructUtil(in, post, 0, n - 1, &postIndex);
 }
 
-// Function to print nodes of a binary tree in BFS order
 void printBFS(struct TreeNode* root) {
     if (root == NULL)
         return;
 
-    // Create a queue for BFS
     struct TreeNode* queue[100];
     int front = 0, rear = 0;
     queue[rear++] = root;
@@ -71,16 +64,13 @@ void printBFS(struct TreeNode* root) {
     }
 }
 
-// Main function
 int main() {
     int inOrder[] = {5, 10, 15, 20, 25, 30, 45};
     int postOrder[] = {5, 15, 10, 25, 45, 30, 20};
     int n = sizeof(inOrder) / sizeof(inOrder[0]);
 
-    // Construct BST
     struct TreeNode* root = bst_construct(inOrder, postOrder, n);
 
-    // Print BST in BFS order
     printf("BFS traversal of constructed BST: ");
     printBFS(root);
     printf("\n");
